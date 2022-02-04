d3ad794538af66927166ef7af0bbaf543e835d7a
Matheus Laurindo <matheus104521@gmail.com>
2022-02-04 20:46:26 -0300

    Adiciona validacao de email

--- schema/UserSchema.js
+++ schema/UserSchema.js
@@ -1,15 +1,24 @@
 const errors = {
   displayNameLength: '"displayName" length must be at least 8 characters long',
+  validEmail: '"email" must be a valid email',
 };
 
 const isLengthLetterThan = (value, min) => (value.length < min);
+const validateEmailMask = (email) => {
+  const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+  if (regex.test(email)) return true;
 
-const validate = (displayName, _email, _password, _image) => {
+  return false;
+};
+
+const validate = (displayName, email, _password, _image) => {
   const code = 400;
 
   switch (true) {
     case isLengthLetterThan(displayName, 8):
-      return { code, message: errors.displayNameLength };  
+      return { code, message: errors.displayNameLength };
+    case !validateEmailMask(email):
+      return { code, message: errors.validEmail };
     default:
       return {};
   }
