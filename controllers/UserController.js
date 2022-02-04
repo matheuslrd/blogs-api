0427013ef1ad5d06b9b20c8bf968dbbb14ab66f5
Matheus Laurindo <matheus104521@gmail.com>
2022-02-04 16:24:07 -0300

    Adiciona rota basica de criaçao de usuario e adiciona o body-parser como dependencia de desenvolvimento

---
+++ controllers/UserController.js
@@ -0,0 +1,5 @@
+const createUser = (_req, res) => res.status(200).send('Bora criar um usuário?');
+
+module.exports = {
+  createUser,
+};
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { createUser } = require('./controllers/UserController');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
@@ -8,3 +12,6 @@ app.listen(3000, () => console.log('ouvindo porta 3000!'));
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.route('/user')
+  .post(createUser);
